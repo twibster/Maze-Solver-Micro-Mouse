2d734146bd37d50b5d52e2d57c85417203f695ef
Omar <79904888+twibster@users.noreply.github.com>
2022-08-03 00:32:19 +0300

    fix the sudden change in position in DFS output
    record path history to reverse the movement
    use global variable (temporarily) to record the history
    fix some bug the maze printing function
    handle input errors for the "surr" variable
    the next movement direction is yet to be coded

--- explorer/base.py
+++ explorer/base.py
@@ -1,97 +1,103 @@
 from copy import deepcopy
 
-##### Start of DFS vertex object
+# Start of DFS vert object
+
+
 class vertex:
-	def __init__(self,position,maze):
-		self.position = position
-		self.maze = maze
-		self.start = False
-		self.visited = False
-		self.updated = False
+    def __init__(self, position, maze):
+        self.position = position
+        self.maze = maze
+        self.start = False
+        self.visited = False
+        self.updated = False
 
-	def connect(self):
-		self.right = self.maze[self.position[0]][self.position[1]+1] if self.position[1] < len(self.maze)-1 else None
-		self.up = 	 self.maze[self.position[0]-1][self.position[1]] if self.position[0] > 0 else None
-		self.left =  self.maze[self.position[0]][self.position[1]-1] if self.position[1] > 0 else None
-		self.down=   self.maze[self.position[0]+1][self.position[1]] if self.position[0] < len(self.maze)-1 else None
-		return 
+    def connect(self):
+        self.right = self.maze[self.position[0]][self.position[1]+1] if self.position[1] < len(self.maze)-1 else None
+        self.up = self.maze[self.position[0]-1][self.position[1]] if self.position[0] > 0 else None
+        self.left = self.maze[self.position[0]][self.position[1]-1] if self.position[1] > 0 else None
+        self.down = self.maze[self.position[0]+1][self.position[1]] if self.position[0] < len(self.maze)-1 else None
+        return 
 
-	def neighbors(self):
-		return self.right,self.up,self.left,self.down
+    def neighbors(self):
+        return self.right,self.up,self.left,self.down
 
-	def deal_neighbor(self,neighbor,command):
-		'''this method does what the command says to the given neighbor:
-		   command --> 0 returns the neighbor vertex direction
-		   command --> 1 clears the neighbor vertex'''
+    def deal_neighbor(self, neighbor, command):
+        '''this method does what the command says to the given neighbor:
+            command --> 0 returns the neighbor vertex direction
+            command --> 1 clears the neighbor vertex'''
 
-		directions ={0:"up",1:"left",2:"down",3:"right"}
+        directions = {0: "up", 1: "left", 2: "down", 3: "right"}
 
-		for i in range(4):
-			if getattr(self,directions[i])==neighbor:
-				if command == 0:
-					return i
-				else:
-					setattr(self,directions[i],None)
+        for i in range(4):
+            if getattr(self, directions[i]) == neighbor:
+                if command == 0:
+                    return i
+                setattr(self, directions[i], None)
+        return None
 
-	def __repr__(self):
-		return f"vertex {self.position}"
+    def __repr__(self):
+        return f"vertex {self.position}"
 
-	def __str__(self):
-		return f"vertex {self.position}"
+    def __str__(self):
+        return f"vertex {self.position}"
 
-##### End of DFS vertex object
+# End of DFS vertex object
 
-##### Start of maze functions
+
+# Start of maze functions
+
 def maze_creator(rows,cols):
-	maze = [[0,0,0,0,0,0,0,0,0], # 9x9
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0],
-		   [0,0,0,0,0,0,0,0,0]] ## this is temporary (hard-coded for trial)
+    maze = [[0,0,0,0,0,0,0,0,0],  # 9x9
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0]]  ## this is temporary (hard-coded for trial)
 
-	for row in range(rows):
-		for col in range(cols):
-			maze[row][col]=vertex((row,col),maze)
+    for row in range(rows):
+        for col in range(cols):
+            maze[row][col] = vertex((row, col), maze)
 
-	maze_connector(maze)
-	return maze
+    maze_connector(maze)
+    return maze
+
 
 def maze_connector(maze):
-	'''this function connects each vertex with its surrounding vertices'''
-	for row in range(len(maze)):
-		for col in range(len(maze)):
-			vertex = maze[row][col]
-			vertex.connect() if vertex else None
+    '''this function connects each vertex with its surrounding vertices'''
+    for row in range(len(maze)):
+        for col in range(len(maze)):
+            vertex = maze[row][col]
+            vertex.connect() if vertex else None
 
-def maze_printer(maze,status,current_vertex=None,orien=None):
-	'''this functions prints the maze given 2 parameters:
-	maze: the maze itself
-	status: 0 --> raw maze (the path is shown as vertices and the walls as None)
-			1(default) --> cleared maze (the path is shown as empty place and the walls as #)'''
-	direction_symbol={0:"|^",1:"<-",2:"| ",3:"->"} # symbols to represent the orientation
-	temp_maze = deepcopy(maze)
-	if not status:
-		for row in range(len(maze)):
-			for col in range(len(maze)):
-				vertex = maze[row][col]
-				if vertex:
-					if vertex.start:
-						temp_maze[row][col]= "S "
-					elif vertex == current_vertex:
-						temp_maze[row][col]= direction_symbol[orien]
-					elif vertex.visited:
-						temp_maze[row][col]= "/ "
-					else:
-						temp_maze[row][col]= "  "
-				else:
-					temp_maze[row][col]= "# "
 
-	for row in range(len(maze)):
-		print(temp_maze[row])
-	print("\n ---------------------")
+def maze_printer(maze, status, current_vertex=None, orien=None):
+    '''this functions prints the maze given 2 parameters:
+    maze: the maze itself
+    status: 0 --> raw maze (the path is shown as vertices and the walls as None)
+    1 (default) --> cleared maze (the path is shown as empty place and the walls as'''
 
-#### End of maze functions
+    direction_symbol = {0: "|^", 1: "<-", 2: "||", 3: "->"}  # symbols to represent the orientation
+    temp_maze = deepcopy(maze)
+    if not status:
+        for row in range(len(maze)):
+            for col in range(len(maze)):
+                vertex = maze[row][col]
+                if vertex:
+                    if vertex == current_vertex:
+                        temp_maze[row][col] = direction_symbol[orien]
+                    elif vertex.start:
+                        temp_maze[row][col] = "S "
+                    elif vertex.visited:
+                        temp_maze[row][col] = "/ "
+                    else:
+                        temp_maze[row][col] = "  "
+                else:
+                    temp_maze[row][col] = "# "
+
+    for row in range(len(maze)):
+        print(temp_maze[row])
+
+# End of maze functions
--- explorer/maze_solver.py
+++ explorer/maze_solver.py
@@ -1,67 +1,105 @@
-from base import *
 import os
+import time
+from base import maze_creator, maze_connector, maze_printer
 
-def where_to_go(where_am_i,orien,surr,maze,dfs_history,start=False):
-	'''this function returns the direction of movement given three variables:
-	   where_am_i = a tuple of x,y --> (x,y)
-	   orien = from 0 to 4 (the absolute orientation is facing forward (0) (1 --> left) (2 --> backward) (3 --> right))
-	   surr = a tuple of the surrounding blocks (up,left,down,right) --> 1 if the block is clear (can move there),
-		      0 or None if a wall is detected.'''
 
-	# where_am_i = calibrate_tuple(where_am_i,1) #reverse the x , y coordinates 
-	current_vertex = maze[where_am_i[0]][where_am_i[1]]
-	if not current_vertex: # hopefully this condition will not be met (the robot got through one of the walls)
-		return 0
-	if start:
-		current_vertex.start=True
-	surr = calibrate_tuple(surr,orien)
-	update_vertex(current_vertex,surr,maze)
-	dfs(current_vertex,dfs_history,maze)
-	return 
+def localize(where_am_i, orien, maze, start=False, surr=None):
+    '''this function returns the direction of movement given three variables:
+       where_am_i = a tuple of x,y --> (x,y)
+       orien = from 0 to 4 (the absolute orientation is facing forward (0) (1 --> left) (2 --> backward) (3 --> right))
+       surr = a tuple of the surrounding blocks (up,left,down,right) --> 1 if the block is clear (can move there),
+        0 or None if a wall is detected.'''
+    # where_am_i = calibrate_tuple(where_am_i,1) #reverse the x , y coordinates
+  
+    current_vertex = maze[where_am_i[0]][where_am_i[1]]
+    os.system('cls')
+    maze_printer(maze, 0, current_vertex, orien=orien)
 
-def calibrate_tuple(to_rotate,factor):
-	'''this function rotates a tuple (surr),(where_am_i) by a given number(orien),(1)in order to align the micro-mouse with the maze'''
+    if not current_vertex.visited:
+        current_vertex.visited = True
+        if start:
+            current_vertex.start = current_vertex.updated = True
 
-	while factor>=len(to_rotate):
-	    factor = factor - len(to_rotate)
-	return to_rotate[-factor:] + to_rotate[:-factor]
+        history.append(current_vertex)
+        print(f"\nPath history: {history}\nOrientation: {orien}\n")
 
-def update_vertex(vertex,surr,maze):
-	'''this function updates each vertex with the correct information received from the micro-mouse
-	   whether it is a wall or not --We assumed earlier that all vertices in the maze are open (not walls)'''
+        if not surr:
+            error = True
+            while error:
+                try:
+                    surr = tuple(map(int, [ch for ch in input("Enter the surroundings: ")]))  # get the surroundings from the user (these will be obtained from the sensors later)    
+                except ValueError:
+                    print("\nOnly numbers are accepted")
+                else:
+                    if len(surr) < 4:
+                        print("\n4 numbers at least required")
+                    else:
+                        error = False    
+        surr = calibrate_tuple(surr, orien)
+        update_vertex(current_vertex, surr, maze)
+        dfs(current_vertex, maze)
 
-	directions ={0:"up",1:"left",2:"down",3:"right"}
-	for i in range(4):
-		neighbor_vertex = getattr(vertex,directions[i])
-		if neighbor_vertex:
-			if not neighbor_vertex.updated:
-				if not (neighbor_vertex and surr[i]): # check if the received status is the same as the assumed one 
-					maze[neighbor_vertex.position[0]][neighbor_vertex.position[1]] = None # set the block as a wall
-				neighbor_vertex.updated = True # flag the vertex as updated (future updates will be ignored)
-	maze_connector(maze)
+    time.sleep(0.5)
 
-def dfs(vertex,history,maze):
-	if vertex.start:
-		start=vertex
-		vertex.updated = vertex.visited = True
-		history.append(vertex)
 
-	for neighbor in vertex.neighbors():
-		if neighbor and (not neighbor.visited):
-			neighbor.visited = True
-			history.append(neighbor) 
-			orein = vertex.deal_neighbor(neighbor,0) # get the direction to the next vertex
-			os.system("cls")
-			print(history,orein)
-			maze_printer(maze,0,neighbor,orien=orein)
-			surr=tuple(map(int,input("Data: ").split(" "))) # get the surroundings from the user (these will be obtained from the sensors later)
-			where_to_go(neighbor.position,orein,surr,maze,history)
-	return 1
+def calibrate_tuple(to_rotate, factor):
+    '''this function rotates a tuple (surr),(where_am_i) by a given number(orien),(1)
+    in order to align the micro-mouse with the maze'''
+
+    while factor >= len(to_rotate):
+        factor = factor - len(to_rotate)
+    return to_rotate[-factor:] + to_rotate[:-factor]
+
+
+def update_vertex(vertex, surr, maze):
+    '''this function updates each vertex with the correct information received from the micro-mouse
+       whether it is a wall or not --We assumed earlier that all vertices in the maze are open (not walls)'''
+
+    directions = {0: "up", 1: "left", 2: "down", 3: "right"}
+    for i in range(4):
+        neighbor_vertex = getattr(vertex,directions[i])
+        if neighbor_vertex:
+            if not neighbor_vertex.updated:
+                if not (neighbor_vertex and surr[i]):  # check if the received status is the same as the assumed one 
+                    maze[neighbor_vertex.position[0]][neighbor_vertex.position[1]] = None  # set the block as a wall
+                neighbor_vertex.updated = True  # flag the vertex as updated (future updates will be ignored)
+    maze_connector(maze)  # reconnect the vertices
+
+
+def dfs(vertex,maze):
+    '''this function implements DFS to make sure each block is visited (it points to the next block to be visited)'''
+    for neighbor in vertex.neighbors():
+        if neighbor and (not neighbor.visited):
+            from_to(vertex, neighbor, maze)
+    return vertex
+
+
+def from_to(location, destination, maze, from_dfs=1):
+    global history
+    directions = {0: "in front of you", 1: "to your left", 2: "behind you", 3: "to your right"}
+    condition = destination in history[-1].neighbors() if from_dfs else destination in location.neighbors()
+    print(f"I was in {location} and moved to {destination}\n")
+    if condition:
+        direction = location.deal_neighbor(destination, 0)
+        return localize(destination.position, direction, maze)
+
+    reversed_path = list(reversed(history[history.index(location):-1]))
+    location = history[-1]
+
+    for prev_vertex in reversed_path:
+        from_to(location, prev_vertex, maze, 0)
+        location = prev_vertex
+    history = history[:history.index(location)+1]
+    return from_to(location, destination, maze)
+
 
 def main():
-	maze = maze_creator(9,9)
-	where_to_go((0,1),2,(1,0,1,0),maze,[],1) #initiate the first step
-	print("the maze has been explored or a loop messed me up")
-	maze_printer(maze,0,vertex)
+    global history
+    history = []
+    maze = maze_creator(9, 9)
+    localize((0, 1), 2, maze, True)  # initiate the first step
+    print("the maze has been explored or a loop messed me up\n")
+    maze_printer(maze, 0)
+
 
 main()
