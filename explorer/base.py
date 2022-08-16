79bfb71967cdd7bccde4636f9938acf2bd71f444
Omar <79904888+twibster@users.noreply.github.com>
2022-08-16 03:09:57 +0300

    use maze class, guide the robot when solving
    known issues:
    1- flood fill function does not work as expected (does not always find the fastest path)
    2- still needs some optimizations

--- explorer/base.py
+++ explorer/base.py
@@ -2,23 +2,86 @@ import random
 from copy import deepcopy
 
 
+class Maze:
+    def __init__(self, array):
+        self.array = array
+        self.start = None
+        self.end = None
+
+    def fill(self):
+        for row in range(len(self.array)):
+            for col in range(len(self.array)):
+                self.array[row][col] = Vertex((row, col), self)
+
+    def connect(self):
+        '''this function connects each vertex with the surrounding vertices'''
+        for row in range(len(self.array)):
+            for col in range(len(self.array)):
+                vertex = self.array[row][col]
+                vertex.connect() if vertex else None
+
+    def print(self, status, current_vertex=None, orien=None):
+        '''this functions prints the maze given 2 parameters:
+        maze: the maze itself
+        status: 0 --> weighted maze (the order of the vertices is shown (flood-fill) )
+        1 (default) --> cleared maze (the path is shown as empty place and the walls as #'''
+
+        direction_symbol = {0: "|^", 1: "<-", 2: "||", 3: "->"}  # symbols to represent the orientation
+        temp_maze = deepcopy(self.array)
+
+        if status:
+            for row in range(len(self.array)):
+                for col in range(len(self.array)):
+                    vertex = self.array[row][col]
+                    if vertex:
+                        if len(str(vertex.order))==2:
+                            temp_maze[row][col] = str(vertex.order)
+                        elif len(str(vertex.order))==1:
+                            temp_maze[row][col] = f"{vertex.order} "
+                        else:
+                            temp_maze[row][col] = "# "
+                    else:
+                        temp_maze[row][col] = "# "
+
+        else:
+            for row in range(len(self.array)):
+                for col in range(len(self.array)):
+                    vertex = self.array[row][col]
+                    if vertex:
+                        if vertex == current_vertex:
+                            temp_maze[row][col] = direction_symbol[orien]
+                        elif vertex == self.start:
+                            temp_maze[row][col] = "S "
+                        elif vertex == self.end:
+                            temp_maze[row][col] = "E "
+                        elif vertex.visited:
+                            temp_maze[row][col] = "/ "
+                        else:
+                            temp_maze[row][col] = "  "
+                    else:
+                        temp_maze[row][col] = "# "
+
+
+        for row in range(len(self.array)):
+            print(temp_maze[row])
+
+
 # Start of DFS vert object
 
 
-class vertex:
+class Vertex:
     def __init__(self, position, maze):
         self.position = position
         self.maze = maze
-        self.start = False
         self.visited = False
         self.updated = False
         self.order = None
 
     def connect(self):
-        self.right = self.maze[self.position[0]][self.position[1]+1] if self.position[1] < len(self.maze)-1 else None
-        self.up = self.maze[self.position[0]-1][self.position[1]] if self.position[0] > 0 else None
-        self.left = self.maze[self.position[0]][self.position[1]-1] if self.position[1] > 0 else None
-        self.down = self.maze[self.position[0]+1][self.position[1]] if self.position[0] < len(self.maze)-1 else None
+        self.right = self.maze.array[self.position[0]][self.position[1]+1] if self.position[1] < len(self.maze.array)-1 else None
+        self.up = self.maze.array[self.position[0]-1][self.position[1]] if self.position[0] > 0 else None
+        self.left = self.maze.array[self.position[0]][self.position[1]-1] if self.position[1] > 0 else None
+        self.down = self.maze.array[self.position[0]+1][self.position[1]] if self.position[0] < len(self.maze.array)-1 else None
         return 
 
     def neighbors(self):
@@ -45,85 +108,3 @@ class vertex:
         return f"vertex {self.position}"
 
 # End of DFS vertex object
-
-
-# Start of maze functions
-
-def maze_creator(rows,cols):
-    maze = [[0,0,0,0,0,0,0,0,0],  # 9x9
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0],
-           [0,0,0,0,0,0,0,0,0]]  ## this is temporary (hard-coded for trial)
-
-    for row in range(rows):
-        for col in range(cols):
-            maze[row][col] = vertex((row, col), maze)
-
-    maze_connector(maze)
-    return maze
-
-
-def maze_connector(maze):
-    '''this function connects each vertex with its surrounding vertices'''
-    for row in range(len(maze)):
-        for col in range(len(maze)):
-            vertex = maze[row][col]
-            vertex.connect() if vertex else None
-
-def get_maze_end(maze):
-    '''this function returns the end vertix'''
-    for row in range(len(maze)):
-        for col in range(len(maze)):
-            vertex = maze[row][col]
-            if vertex:
-                if vertex.order==0:
-                    return vertex
-            
-def maze_printer(maze, status, current_vertex=None, orien=None):
-    '''this functions prints the maze given 2 parameters:
-    maze: the maze itself
-    status: 0 --> weighted maze (the order of the vertices is shown (flood-fill) )
-    1 (default) --> cleared maze (the path is shown as empty place and the walls as #'''
-
-    direction_symbol = {0: "|^", 1: "<-", 2: "||", 3: "->"}  # symbols to represent the orientation
-    temp_maze = deepcopy(maze)
-
-    if status:
-        for row in range(len(maze)):
-            for col in range(len(maze)):
-                vertex = maze[row][col]
-                if vertex:
-                    if len(str(vertex.order))==2:
-                        temp_maze[row][col] = str(vertex.order)
-                    elif len(str(vertex.order))==1:
-                        temp_maze[row][col] = f"{vertex.order} "
-                    else:
-                        temp_maze[row][col] = "# "
-                else:
-                    temp_maze[row][col] = "# "
-    else:
-        for row in range(len(maze)):
-            for col in range(len(maze)):
-                vertex = maze[row][col]
-                if vertex:
-                    if vertex == current_vertex:
-                        temp_maze[row][col] = direction_symbol[orien]
-                    elif vertex.start:
-                        temp_maze[row][col] = "S "
-                    elif vertex.visited:
-                        temp_maze[row][col] = "/ "
-                    else:
-                        temp_maze[row][col] = "  "
-                else:
-                    temp_maze[row][col] = "# "
-
-
-    for row in range(len(maze)):
-        print(temp_maze[row])
-
-# End of maze functions
--- explorer/maze_solver.py
+++ explorer/maze_solver.py
@@ -1,9 +1,9 @@
 import os
 import time
-from base import maze_creator, maze_connector, maze_printer,get_maze_end
+from base import Maze
 
 
-def localize(where_am_i, orien, maze, start=False, surr=None):
+def localize(where_am_i, orien, maze, start=False, surr=None, run=0):
     '''this function returns the direction of movement given three variables:
        where_am_i = a tuple of x,y --> (x,y)
        orien = from 0 to 4 (the absolute orientation is facing forward (0) (1 --> left) (2 --> backward) (3 --> right))
@@ -12,13 +12,21 @@ def localize(where_am_i, orien, maze, start=False, surr=None):
 
     # where_am_i = calibrate_tuple(where_am_i,1) #reverse the x , y coordinates
   
-    current_vertex = maze[where_am_i[0]][where_am_i[1]]
-    maze_printer(maze, 0, current_vertex, orien=orien)
+    current_vertex = maze.array[where_am_i[0]][where_am_i[1]]
+    maze.print(0, current_vertex, orien)
 
+    if run:
+        for neighbor in current_vertex.neighbors():
+            if neighbor:
+                if not (current_vertex.order - (neighbor.order+1)):
+                    time.sleep(1)
+                    from_to(current_vertex, neighbor, orien, maze, 0, run=1)
+    
     if not current_vertex.visited:
         current_vertex.visited = True
         if start:
-            current_vertex.start = current_vertex.updated = True
+            maze.start = current_vertex
+            current_vertex.updated = True
 
         history.append(current_vertex)
         print(f"\nPath history: {history}\nOrientation: {orien}\n")
@@ -30,6 +38,7 @@ def localize(where_am_i, orien, maze, start=False, surr=None):
                     surr = tuple(map(int, [ch for ch in input("Enter the surroundings: ")]))  # get the surroundings from the user (these will be obtained from the sensors later)
                     if len(surr) >4:
                         current_vertex.order = 0
+                        maze.end = current_vertex
                         surr = surr[:-1]
                 except ValueError:
                     print("\nOnly numbers are accepted")
@@ -66,27 +75,29 @@ def update_vertex(vertex, surr, maze):
         if neighbor_vertex:
             if not neighbor_vertex.updated:
                 if not (neighbor_vertex and surr[i]):  # check if the received status is the same as the assumed one 
-                    maze[neighbor_vertex.position[0]][neighbor_vertex.position[1]] = None  # set the block as a wall
+                    maze.array[neighbor_vertex.position[0]][neighbor_vertex.position[1]] = None  # set the block as a wall
                 neighbor_vertex.updated = True  # flag the vertex as updated (future updates will be ignored)
-    maze_connector(maze)  # reconnect the vertices
+    maze.connect()  # reconnect the vertices
 
 
 def dfs(vertex,orien,maze):
     '''this function implements DFS to make sure each block is visited (it points to the next block to be visited)'''
     for neighbor in vertex.neighbors():
         if neighbor and (not neighbor.visited):
-            from_to(vertex, neighbor,orien, maze)
+            from_to(vertex, neighbor, orien, maze)
     return vertex
 
 
-def from_to(location, destination,orien, maze, from_dfs=1):
+def from_to(location, destination,orien, maze, from_dfs=1, run=0):
+    
     global history
     directions = ("in front of me","to my left","behind me","to my right")
     condition = destination in history[-1].neighbors() if from_dfs else destination in location.neighbors()
+
     if condition:
         direction = location.deal_neighbor(destination, 0)
         print(f"I am in {location} and moving to {destination} which is {calibrate_tuple(directions, orien)[direction]} {orien}\n")
-        return localize(destination.position, direction, maze)
+        return localize(destination.position, direction, maze, run=run)
 
     reversed_path = list(reversed(history[history.index(location):-1]))
     location = history[-1]
@@ -96,25 +107,42 @@ def from_to(location, destination,orien, maze, from_dfs=1):
         location = prev_vertex
 
     history = history[:history.index(location)+1]
-    return from_to(location, destination,orien, maze)
+    return from_to(location, destination, orien, maze)
+
 
 def flood_fill(maze,vertex,order):
+    '''this function flood-fills the maze to get the shortest path from start to end only'''
     for neighbor in vertex.neighbors():
         if neighbor:
             if neighbor.order == None:
                 neighbor.order = order +1
-                flood_fill(maze,neighbor,neighbor.order)
+                flood_fill(maze, neighbor, neighbor.order)
     return
 
+
 def main():
+    array= [[0,0,0,0,0,0,0,0,0],  # 9x9
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0,0]]  ## this is temporary (hard-coded for trial)
+
+    maze = Maze(array)
+    maze.fill()
+    maze.connect()
+
     global history
     history = []
-    maze = maze_creator(9, 9)
-    start = (8, 1)
-    localize(start, 0, maze, True)  # initiate the first step
-    print("Flood-filling the maze\n")
-    flood_fill(maze,get_maze_end(maze),0)
-    maze_printer(maze, 1)
 
-
+    localize((8,1), 0, maze, True)  # initiate the first step
+    print("Flood-filling the maze:\n")
+    flood_fill(maze, maze.end, 0)
+    maze.print(1)
+    print("\n Solving the maze\n")
+    localize(maze.start.position, 0, maze, run=1)
+    
 main()
